dd33574c15cd81f636219188aa2f72cf34bde48f
renan ribeiro <renan.admribeiro@gmail.com>
2021-10-06 15:07:12 -0300

    fazendo primeiro teste(falho)

---
+++ src/util/module-alias.ts
@@ -0,0 +1,8 @@
+import path from 'path'
+import {addAliases} from 'module-alias'
+
+const files = path.resolve(__dirname,'../..')
+addAliases({
+    "@src":path.join(files,"src"),
+    "@test":path.join(files,"test")
+})
